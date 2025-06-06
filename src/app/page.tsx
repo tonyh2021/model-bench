510d45aa1a065a9e71e6aecca4a7ebd9daef0038
Tony <tonyhx2021@gmail.com>
2025-06-06 14:36:54 -0400

    add index page.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Dashboard } from "@/components/Dashboard";
+
+export default function Home() {
+  return (
+    <main className="mobile-text smooth-scroll mobile-container bg-background min-h-screen px-2 py-2 sm:px-4 sm:py-4 lg:px-6 xl:px-8">
+      {/* <Dashboard /> */}
+      <p>Hello</p>
+    </main>
+  );
+}
