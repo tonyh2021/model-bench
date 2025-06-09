5eec5a6e7f35d7426a6e54dbe3fff38afe1a636b
Tony <tonyhx2021@gmail.com>
2025-06-09 16:00:09 -0400

    add Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Dashboard } from "@/components/Dashboard";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="mobile-text smooth-scroll mobile-container bg-background min-h-screen px-2 py-2 sm:px-4 sm:py-4 lg:px-6 xl:px-8">
-      <Dashboard />
+    <main className="mobile-text smooth-scroll mobile-container min-h-screen bg-background px-2 py-2 sm:px-4 sm:py-4 lg:px-6 xl:px-8">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Dashboard />
+      </Suspense>
     </main>
   );
 }
